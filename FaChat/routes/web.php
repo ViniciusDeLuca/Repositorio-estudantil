0bb51399c5405c1cfc16c84ce184a1dbb28883f3
VDLuca <viniciusdeluca3@gmail.com>
2023-03-16 20:54:05 -0300

    Rotas de login e cadastro

--- FaChat/routes/web.php
+++ FaChat/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('login', [LoginController::class, 'redirecionarLogin'])->name('login');
+Route::get('logar', [LoginController::class, 'logar'])->name('logar');
+Route::get('cadastro', [LoginController::class, 'redirecionarCadastro'])->name('redirecionarCadastro');
