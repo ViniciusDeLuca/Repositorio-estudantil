3b99d053c6338913e9450350c93e3a00994bac65
Vinícius De Luca <viniciusdeluca3@gmail.com>
2023-05-01 09:37:46 -0300

    Correção no boot do app

--- FaChat/app/Providers/AppServiceProvider.php
+++ FaChat/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if(env('APP_ENV') != 'local')
+        {
+            \URL::forceScheme('https');
+        }
+
+        Schema::defaultStringLength(191);
+
     }
 }
