ee60aed08cc2b0bd1566059331d766ff0277243b
Vinícius De Luca <viniciusdeluca3@gmail.com>
2023-05-01 21:25:38 -0300

    Removendo centralizacao do texto

--- FaChat/resources/views/publicacao.blade.php
+++ FaChat/resources/views/publicacao.blade.php
@@ -1,13 +1,13 @@
 @extends('inicio')
 @section('content')
 <div class="container mt-5">
-    <div class="card text-center mb-3">
-        <div class="card-header">
+    <div class="card mb-3">
+        <div class="card-header text-center">
           {{ $publicacao->materia->nome }} - {{ $publicacao->autor->nome . " " . $publicacao->autor->sobrenome}}
         </div>
         <div class="card-body">
           <h5 class="card-title">{{ $publicacao->titulo }}</h5>
-          <p class="card-text text-start">{!! $publicacao->conteudo !!}</p>
+          <p class="card-text">{!! $publicacao->conteudo !!}</p>
           @if($publicacao->data_entrega)
             <p class="card-text text-start text-danger">Data de entrega: {{ date('d/m/Y',strtotime($publicacao->data_entrega)) }}</p>
           @endif
